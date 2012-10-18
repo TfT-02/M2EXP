7a9c80bed43c2a2c75761b757b7f7a7d326de1d0
computerdude5000 <computerdude5000@gmail.com>
2012-10-18 08:32:17 -0400

    GO GPL!!!

--- src/main/java/com/github/computerdude5000/m2exp/commands/Buyarchery.java
+++ src/main/java/com/github/computerdude5000/m2exp/commands/Buyarchery.java
@@ -1,3 +1,20 @@
+/**	
+ * M2EXP - Buyarchery.java
+ * Copyright (c) 2012 Computerdude5000, <computerdude5000@gmail.com>	
+ *	
+ * M2EXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or	
+ * (at your option) any later version.
+ *	
+ * M2EXP is distributed in the hope that it will be useful,	
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of	
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with M2EXP.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.computerdude5000.m2exp.commands;
 
 import net.milkbowl.vault.economy.EconomyResponse;
