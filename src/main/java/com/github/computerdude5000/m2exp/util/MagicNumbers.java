579f47e3414cf97ebcdf51f52493b36f99a74b2d
computerdude5000 <computerdude5000@gmail.com>
2013-04-02 16:57:59 -0400

    magic

--- src/main/java/com/github/computerdude5000/m2exp/util/MagicNumbers.java
+++ src/main/java/com/github/computerdude5000/m2exp/util/MagicNumbers.java
@@ -1,7 +1,6 @@
 package com.github.computerdude5000.m2exp.util;
-
-public class MagicNumbers {
-
+ public final class MagicNumbers {
+	public static final int l5 = 5 ;
 	public static final int l10 = 10;
 	public static final int l20 = 20;
 	public static final int l30 = 30;
