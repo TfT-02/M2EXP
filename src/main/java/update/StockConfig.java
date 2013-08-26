22afca37f7d81949ec457a3d85de85d49ae960ca
computerdude5000 <computerdude5000@gmail.com>
2013-08-25 23:22:46 -0400

    grrrr not this again.

--- src/main/java/update/StockConfig.java
+++ src/main/java/update/StockConfig.java
@@ -3,6 +3,15 @@
 */
 package update;
 
+import com.github.computerdude5000.m2exp.M2EXP;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Administrator
@@ -13,4 +22,77 @@ package update;
 
 public class StockConfig
 {
+    private final String fileName;
+    private final M2EXP plugin;
+    private File configFile;
+    private FileConfiguration fileConfiguration;
+
+    public StockConfig(M2EXP plugin, String fileName)
+    {
+        if (plugin == null)
+        {
+            throw new IllegalArgumentException("plugin cannot be null");
+        }
+        if (!plugin.isInitialized())
+        {
+            throw new IllegalArgumentException("plugin must be initiaized");
+        }
+        this.plugin = plugin;
+        this.fileName = fileName;
+        File dataFolder = plugin.getDataFolder();
+        if (dataFolder == null)
+        {
+            throw new IllegalStateException();
+        }
+        this.configFile = new File(plugin.getDataFolder(), fileName);
+    }
+
+    public void reloadConfig()
+    {
+        fileConfiguration = YamlConfiguration.loadConfiguration(configFile);
+
+        // Look for defaults in the jar
+        InputStream defConfigStream = plugin.getResource(fileName);
+        if (defConfigStream != null)
+        {
+            YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            fileConfiguration.setDefaults(defConfig);
+        }
+    }
+
+    public FileConfiguration getConfig()
+    {
+        if (fileConfiguration == null)
+        {
+            this.reloadConfig();
+        }
+        return fileConfiguration;
+    }
+
+    public void saveConfig()
+    {
+        if (fileConfiguration == null || configFile == null)
+        {
+            return;
+        } else
+        {
+            try
+            {
+                getConfig().save(configFile);
+            } catch (IOException ex)
+            {
+                plugin.getLogger().log(Level.SEVERE, "Could not save config to " + configFile, ex);
+            }
+        }
+    }
+
+    public void saveDefaultConfig()
+    {
+        if (!configFile.exists())
+        {
+            this.plugin.saveResource(fileName, false);
+        }
+    }
+
 }
+
