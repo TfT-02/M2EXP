e4fbccab3c638701b26ca938cc8c69ad018daeb8
computerdude5000 <computerdude5000@gmail.com>
2013-11-11 10:37:53 -0500

    Worked a little bit more on the Buy and Sell commands added help dialog for both.

--- src/main/java/update/commands/Buy.java
+++ src/main/java/update/commands/Buy.java
@@ -19,30 +19,38 @@ import org.bukkit.entity.Player;
 
 public class Buy implements CommandExecutor
 {
-    public boolean onCommand( CommandSender commandSender, Command command, String s, String[] strings )
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
     {
-        if ( commandSender instanceof Player )
+        if (commandSender instanceof Player)
         {
 
             Player player = (Player) commandSender;
-            if ( command.getName().equalsIgnoreCase( "buy" ) )
+            if (command.getName().equalsIgnoreCase("buy"))
             {
-                if ( strings[0].equalsIgnoreCase( "cash" ) || strings[0].equalsIgnoreCase( "money" ) )
+                if (strings[0].equalsIgnoreCase("cash") || strings[0].equalsIgnoreCase("money"))
                 {
+                    //TODO add code here that will check if it is a number
                     return true;
-                } else if ( strings[0].equalsIgnoreCase( "exp" ) || strings[0].equalsIgnoreCase( "xp" ) )
+                } else if (strings[0].equalsIgnoreCase("exp") || strings[0].equalsIgnoreCase("xp"))
                 {
+                    //TODO add code here that will check if it is a number
                     return true;
+                } else if (strings[0].equalsIgnoreCase("help"))
+                {
+                    player.sendMessage(ChatColor.AQUA + "You should probably check out /stock for current information ");
+                    player.sendMessage(ChatColor.BLUE + "---------formatting---------");
+                    player.sendMessage(ChatColor.GREEN + "command must be like /buy [currency] [skill] [unit amount]");
+                    player.sendMessage(ChatColor.BOLD + "IE:" + ChatColor.stripColor(ChatColor.GREEN + "/buy cash mining 100"));
                 } else
                 {
-                    player.sendMessage( ChatColor.RED + "Invalid: Arguements must be cash, money, exp, xp are the only valid accepted arguements" );
+                    player.sendMessage(ChatColor.RED + "Invalid: Arguments must be help, cash, money, exp, xp are the only valid accepted arguements");
                 }
                 return true;
             }
         } else
         {
-            commandSender.sendMessage( ChatColor.RED + "We're sorry but you are not a player so you are not allowed to do the following commands /buy and /sell" );
-            commandSender.sendMessage( ChatColor.RED + "You however are allowed todo /money2XP, /stock and /stats " );
+            commandSender.sendMessage(ChatColor.RED + "We're sorry but you are not a player so you are not allowed to do the following commands /buy and /sell");
+            commandSender.sendMessage(ChatColor.RED + "You however are allowed todo /money2XP, /stock and /stats ");
             return true;
         }
         return false;  //To change body of implemented methods use File | Settings | File Templates.
--- src/main/java/update/commands/Sell.java
+++ src/main/java/update/commands/Sell.java
@@ -3,6 +3,7 @@
 */
 package update.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,13 +17,40 @@ import org.bukkit.entity.Player;
  * To change this template use File | Settings | File Templates.
  */
 
-public class Sell  implements CommandExecutor
+public class Sell implements CommandExecutor
+{
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
+    {
+        if (commandSender instanceof Player)
         {
-public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
+            Player player = (Player) commandSender;
+            if (command.getName().equalsIgnoreCase("sell"))
+            {
+                if (strings[0].equalsIgnoreCase("cash") || strings[0].equalsIgnoreCase("money"))
+                {
+                    //TODO add code here that will check if it is a number
+                    return true;
+                } else if (strings[0].equalsIgnoreCase("exp") || strings[0].equalsIgnoreCase("xp"))
+                {
+                    //TODO add code here that will check if it is a number
+                    return true;
+                } else if (strings[0].equalsIgnoreCase("help"))
+                {
+
+                    player.sendMessage(ChatColor.AQUA + "You should probably check out /stock for current information ");
+                    player.sendMessage(ChatColor.BLUE + "---------formatting---------");
+                    player.sendMessage(ChatColor.GREEN + "command must be like /sell [currency] [skill] [unit amount]");
+                    player.sendMessage(ChatColor.BOLD + "IE:" + ChatColor.stripColor(ChatColor.GREEN + "/sell cash mining 100"));
+                } else
+                {
+                    player.sendMessage(ChatColor.RED + "Invalid: Arguments must be help, cash, money, exp, xp are the only valid accepted arguements");
+                }
+                return true;
+            }
+        } else
         {
-        if(commandSender instanceof Player){
-        } else{  }
+        }
 
         return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
-        }
